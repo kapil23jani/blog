69bad66774f1f1917212395adf42f9f2c0d7658b
Kapil Jani <kapil23jani@gmail.com>
2020-09-22 10:47:48 +0530

    chnages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
+	skip_before_filter :verify_authenticity_token
+	protect_from_forgery prepend: true, with: :null_session
     
     def authenticate_user!
         @current_user = User.find_by(id: session[:user_id])
