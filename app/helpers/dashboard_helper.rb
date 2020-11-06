1664c25b73cedc4374ded530b0ebbdab9aa4cd85
Kapil Jani <kapil23jani@gmail.com>
2020-11-06 08:40:55 +0530

    chnages

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -7,58 +7,71 @@ module DashboardHelper
 			users = []
 			parent_users = []
 
-			sql = " WITH RECURSIVE r AS ( "+ 
-				"SELECT * FROM users WHERE users.id = #{user.id} AND users.position = 'Left' "+
-				"UNION "+ 
-				"SELECT users.* FROM users JOIN r WHERE r.id = users.sponsered_by_id) "+
-				"SELECT * FROM r"
-				records_array = ActiveRecord::Base.connection.execute(sql)
-				if records_array.present?
-					users << User.where(id: records_array.pluck("id"))
-				end
-				users = users.flatten
-				
-				result << users
-				if users.present?
-					if @user.position == "Left" && User.find_by(id: @user.sponsered_by_id).try(:position) == "Left"
-						if users.select {|user| user.position == "Left" }.present?
-							users.each do |user|
-								sql = " WITH RECURSIVE r AS ( "+ 
-																				"SELECT * FROM users WHERE users.id = #{user.sponsered_by_id} AND users.position = 'Left' "+
-																				"UNION "+ 
-																				"SELECT users.* FROM users JOIN r WHERE r.id = users.sponsered_by_id) "+
-																				"SELECT * FROM r"
-								records_array = ActiveRecord::Base.connection.execute(sql)
-								if records_array.present?
-									parent_users << User.where(id: records_array.pluck("id"))
-								end
-								parent_users = parent_users.flatten
-								if parent_users.present?
-									c = parent_users.select { |c_user| c_user.position == "Left" && c_user.created_at > @user.created_at }
-									result << c
-								end
-							end
-						end
-					else
-						users.each do |user|
-							sql = " WITH RECURSIVE r AS ( "+ 
-																				"SELECT * FROM users WHERE users.id = #{user.sponsered_by_id} AND users.position = 'Right' "+
-																				"UNION "+ 
-																				"SELECT users.* FROM users JOIN r WHERE r.id = users.sponsered_by_id) "+
-																				"SELECT * FROM r"
-								records_array = ActiveRecord::Base.connection.execute(sql)
-								if records_array.present?
-									parent_users << User.where(id: records_array.pluck("id"))
-								end
-								parent_users = parent_users.flatten
-								if parent_users.present?
-									c = parent_users.select { |c_user| c_user.position == "Left" && c_user.created_at > @user.created_at }
-									result << c
-								end
+			if user.present?
+				result << User.where(sponsered_by_id: user.id)
+				if User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id)}.present?
+					result << User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).present? ? User.where(sponsered_by_id: user.id) : nil}
+					if User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id)}}.present?
+						result << User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).present? ? User.where(sponsered_by_id: user.id) : nil}}.present?
+						if User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id)}}}.present?
+							result << User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).map{ |user| User.where(sponsered_by_id: user.id).present? ? User.where(sponsered_by_id: user.id) : nil}}}
 						end
 					end
 				end
-			result.flatten.delete_if { |user| user.id == @user.id}
+			end
+
+			# sql = " WITH RECURSIVE r AS ( "+ 
+			# 	"SELECT * FROM users WHERE users.id = #{user.id} AND users.position = 'Left' "+
+			# 	"UNION ALL "+ 
+			# 	"SELECT users.* FROM users JOIN r WHERE (r.id = users.sponsered_by_id)) "+
+			# 	"SELECT * FROM r "
+			# 	records_array = ActiveRecord::Base.connection.execute(sql)
+			# 	if records_array.present?
+			# 		users << User.where(id: records_array.pluck("id"))
+			# 	end
+			# 	users = users.flatten
+				
+			# 	result << users
+			# 	if users.present?
+			# 		if @user.position == "Left" && User.find_by(id: @user.sponsered_by_id).try(:position) == "Left"
+			# 			if users.select {|user| user.position == "Left" }.present?
+			# 				users.each do |user|
+			# 					sql = " WITH RECURSIVE r AS ( "+ 
+			# 																	"SELECT * FROM users WHERE users.id = #{user.sponsered_by_id} AND users.position = 'Left'"+
+			# 																	"UNION ALL "+ 
+			# 																	"SELECT users.* FROM users JOIN r WHERE (r.id = users.sponsered_by_id)) "+
+			# 																	"SELECT * FROM r"
+			# 					records_array = ActiveRecord::Base.connection.execute(sql)
+			# 					if records_array.present?
+			# 						parent_users << User.where(id: records_array.pluck("id"))
+			# 					end
+			# 					parent_users = parent_users.flatten
+			# 					if parent_users.present?
+			# 						c = parent_users.select { |c_user| c_user.position == "Left" && c_user.created_at > @user.created_at }
+			# 						result << c
+			# 					end
+			# 				end
+			# 			end
+			# 		else
+			# 			users.each do |user|
+			# 				sql = " WITH RECURSIVE r AS ( "+ 
+			# 																	"SELECT * FROM users WHERE users.id = #{user.sponsered_by_id} AND users.position = 'Right'"+
+			# 																	"UNION ALL "+ 
+			# 																	"SELECT users.* FROM users JOIN r WHERE (r.id = users.sponsered_by_id)) "+
+			# 																	"SELECT * FROM r"
+			# 					records_array = ActiveRecord::Base.connection.execute(sql)
+			# 					if records_array.present?
+			# 						parent_users << User.where(id: records_array.pluck("id"))
+			# 					end
+			# 					parent_users = parent_users.flatten
+			# 					if parent_users.present?
+			# 						c = parent_users.select { |c_user| c_user.position == "Left" && c_user.created_at > @user.created_at }
+			# 						result << c
+			# 					end
+			# 			end
+			# 		end
+			# 	end
+			# result.flatten.delete_if { |user| user.id == @user.id}
 		end
 
 	end
