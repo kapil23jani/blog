0f35ebd7c5794663ab4c14ca0f91d5365b38fe5e
Kapil Jani <kapil23jani@gmail.com>
2020-11-21 15:24:54 +0530

    chanegs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -34,12 +34,12 @@ module UsersHelper
 
 
 	def get_users id, position
-		sql = "WITH RECURSIVE child_users(id, sponsered_by_id,h_parent,position,lvl, sponser_id) AS " +
-			"( SELECT id, sponsered_by_id, h_parent, position, sponser_id, 0 FROM users " +
+		sql = "WITH RECURSIVE child_users(id, sponsered_by_id,h_parent,position,lvl) AS " +
+			"( SELECT id, sponsered_by_id, h_parent, position, 0 FROM users " +
 			"WHERE users.id = #{id} " +
-			"UNION ALL SELECT  users.id, users.sponsered_by_id, users.h_parent,users.position, users.sponser_id ,child_users.lvl+1 FROM users JOIN child_users " +
+			"UNION ALL SELECT  users.id, users.sponsered_by_id, users.h_parent,users.position,child_users.lvl+1 FROM users JOIN child_users " +
 			"ON child_users.id = users.h_parent and users.position = '#{position}') " +
-			"SELECT id, sponsered_by_id, h_parent,lvl, sponser_id FROM child_users where position ='#{position}'; "
+			"SELECT id, sponsered_by_id, h_parent,lvl FROM child_users where position ='#{position}'; "
 		records_array = ActiveRecord::Base.connection.execute(sql).to_a
 		if records_array.present?
 			return records_array
