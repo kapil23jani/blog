dfa4e53e362c63bfb21da45bd47bebcc0ddef506
Kapil Jani <kapil23jani@gmail.com>
2020-11-21 17:05:55 +0530

    chnages

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -19,11 +19,6 @@ module AdminHelper
 				right_users << child_right_users if child_right_users.present?
 				right_users = User.where(id: right_users.flatten.uniq, is_invoice_valid: true) if right_users.present?
 				return right_users.try(:count)
-			# elsif type == "inactive_left"
-			# 	count = users.select {|user| user.position == "Left" && user.is_invoice_valid == false}.count
-			# elsif type == "inactive_right"
-			# 	count = users.select {|user| user.position == "Right" && user.is_invoice_valid == false}.count
-			
 			end
 		end
 	end
@@ -31,12 +26,9 @@ module AdminHelper
 
 	def find_left_team user
 		if !user.admin?
-			# check_main_pair = User.where(sponsered_by_id: user.id).pluck(:position).uniq.count
-			# return 0 if check_main_pair == 1 || check_main_pair == 0
 			users = []
 			left_users = get_users(user.id, "Left").present? ?  get_users(user.id, "Left").pluck("id") : nil
-			child_left_users = User.where(sponsered_by_id: get_users(user.id, "Left").pluck("id")).pluck(:id) if left_users.present?
-			
+			child_left_users = get_final_users(User.find left_users[0]).pluck(:id) if left_users.present?
 			left_users << child_left_users if child_left_users.present?
 			left_users = User.where(id: left_users.flatten.uniq) if left_users.present?
 			return left_users.try(:count)
@@ -45,17 +37,12 @@ module AdminHelper
 
 	def find_right_team user
 		if !user.admin?
-			# check_main_pair = User.where(sponsered_by_id: user.id).pluck(:position).uniq.count
-			# return 0 if check_main_pair == 1 || check_main_pair == 0
-			users = []
 			right_users = get_users(user.id, "Right").present? ? get_users(user.id, "Right").pluck("id") : nil
-			child_right_users = User.where(sponsered_by_id: get_users(user.id, "Right").pluck("id")).pluck(:id) if right_users.present?
-			
+			child_right_users = get_final_users(User.find right_users[0]).pluck(:id) if right_users.present?
 			right_users << child_right_users if child_right_users.present?
 			right_users = User.where(id: right_users.flatten.uniq) if right_users.present?
 			return right_users.try(:count)
 		end
-
 	end
 
 
@@ -67,15 +54,13 @@ module AdminHelper
 			users = []
 			left_users = get_users(user.id, "Left").pluck("id")
 			right_users = get_users(user.id, "Right").pluck("id")
-			child_left_users = User.where(sponsered_by_id: get_users(user.id, "Left").pluck("id")).pluck(:id) if left_users.present?
-			child_right_users = User.where(sponsered_by_id: get_users(user.id, "Right").pluck("id")).pluck(:id) if right_users.present?
+			child_left_users = get_final_users(User.find left_users[0]).pluck(:id) if left_users.present?
+			child_right_users = get_final_users(User.find right_users[0]).pluck(:id) if right_users.present?
 			
 			left_users << child_left_users if child_left_users.present?
 			right_users << child_right_users if child_right_users.present?
 			left_users = User.where(id: left_users.flatten.uniq) if left_users.present?
 			right_users = User.where(id: right_users.flatten.uniq) if right_users.present?
-
-
 
 			if type == 1
 				left_final_pairs = left_users.where('created_at BETWEEN ? AND ?', (Date.today.beginning_of_month).to_datetime, (Date.today.beginning_of_month).to_datetime + 10.days).sort
